3f7791f08667741c391fd58f3b2efd8a0a51151a
nabidam <navidabbaspoor@gmail.com>
2024-10-23 13:29:26 +0330

    content and date style

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,21 +9,21 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+<article id="post-<?php the_ID(); ?>" <?php post_class('flex flex-col gap-4'); ?>>
+	<header class="entry-header flex justify-between items-center">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		if (is_singular()):
+			the_title('<h1 class="entry-title">', '</h1>');
+		else:
+			the_title('<h2 class="entry-title mb-4 text-xl font-bold decoration-wavy underline decoration-[#d4d4d4] decoration-2 underline-offset-8"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
 		endif;
 
-		if ( 'post' === get_post_type() ) :
+		if ('post' === get_post_type()):
 			?>
-			<div class="entry-meta">
+			<div class="entry-meta p-1 px-4 border-2 border-gray-500 w-fit rounded-md text-sm">
 				<?php
 				minilog_posted_on();
-				minilog_posted_by();
+				// minilog_posted_by();
 				?>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
@@ -31,27 +31,28 @@
 
 	<?php minilog_post_thumbnail(); ?>
 
-	<div class="entry-content">
+	<div class="entry-content flex flex-col gap-2">
 		<?php
 		the_content(
 			sprintf(
 				wp_kses(
 					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'minilog' ),
+					__('بیشتر<span class="screen-reader-text"> "%s"</span>', 'minilog'),
 					array(
 						'span' => array(
-							'class' => array(),
+							'class' => array("text-xl"),
 						),
 					)
 				),
-				wp_kses_post( get_the_title() )
-			)
+				wp_kses_post(get_the_title())
+			),
+			true
 		);
 
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'minilog' ),
-				'after'  => '</div>',
+				'before' => '<div class="page-links">' . esc_html__('Pages:', 'minilog'),
+				'after' => '</div>',
 			)
 		);
 		?>
